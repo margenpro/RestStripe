d7a93d2b1931f1a2699390554543959113d9f089
Santiago Frias <diuk.frias.santiago@gmail.com>
2021-06-03 13:16:44 -0300

    emails

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,8 @@
 const functions = require("firebase-functions");
 const express = require('express')
 const Stripe = require("stripe")
+const {creds} = require("../creds.json") 
+const nodemailer = require("nodemailer")
 const cors = require("cors");
 
 const stripe = Stripe("sk_test_51Iov1mH463ReSVuZqMseoXxSB8eEtNhIFiMRub8epDcCDD63Ndok1QmBSo1mefIyLPALc7ysxaj4jCqTMNYjRe1i00VSlbri1n")
@@ -11,6 +13,35 @@ const port = 3000
 app.use(cors({origin: true}))
 app.use(express.static("."));
 app.use(express.json());
+
+app.post('/api/email', async (req, res) => {
+
+  let user = req.body.user
+  console.log("AAAAAAAAAAAAAAAAAAAAAAAAaa");
+
+  let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: creds.email, 
+      pass: creds.pass
+    },
+  });
+
+  try {
+    await transporter.sendMail({
+      from: creds.email, // sender address
+      to: user.email, // list of receivers
+      subject: "Successful Registration", // Subject line
+      text: `Hi ${user.username}! Thank you so much for joining us! This are your credentials:\nUsername: ${user.username}\nEmail: ${user.email}`
+    })
+    console.log("Email Sent!");
+  
+  } catch (error) {
+    console.log(error)
+  }
+})
 
 app.post('/api/pay', async (req, res) => {
 
